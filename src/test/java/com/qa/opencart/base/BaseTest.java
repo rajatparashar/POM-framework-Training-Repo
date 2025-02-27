413fad8d461b14b03ef747c94635793cd7559143
Rajat Parashar <rajatparasharan@gmail.com>
2025-02-27 20:59:45 +0530

    added code for selenoid support

--- src/test/java/com/qa/opencart/base/BaseTest.java
+++ src/test/java/com/qa/opencart/base/BaseTest.java
@@ -28,14 +28,15 @@ public class BaseTest {
 	protected RegistrationPage registrationPage;
 	protected NewAccountsPage newAccountsPage;
 
-	@Parameters("browser")
+	@Parameters({ "browser", "browserversion" })
 	@BeforeTest
-	public void setup(String browserName) {
+	public void setup(String browserName, String browserVersion) {
 		df = new DriverFactory();
 		prop = df.initProp();
 
 		if (browserName != null) {
 			prop.setProperty("browser", browserName);
+			prop.setProperty("browserversion", browserVersion);
 		}
 
 		driver = df.initDriver(prop);
